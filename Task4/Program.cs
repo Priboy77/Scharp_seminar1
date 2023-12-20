f8bd03844f6b1f8d869afd1053319e90ad4b0592
Andrey Proskurin <>
2023-12-20 20:46:15 +0300

    сделал четвертое решение

---
+++ Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+int evenCount = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] % 2 == 0)
+    {
+        evenCount++;
+    }
+}
+
+Console.WriteLine("Колличество четных чисел в массиве " + evenCount + "");
