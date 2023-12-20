5e80fd83ad24847f83c25683adde0cdc54cec0fb
Andrey Proskurin <>
2023-12-20 20:43:01 +0300

    Добавил исключения в файл gitignore и сделал второе решение

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите натуральное число: ");
+        int number = int.Parse(Console.ReadLine());
+        int digitsCount = (int)Math.Floor(Math.Log10(number)) + 1;      
+        int[] digits = new int[digitsCount];       
+        int index = digitsCount - 1;
+        while (number > 0)
+        {
+            digits[index] = number % 10;
+            number /= 10;
+            index--;
+        }       
+        Console.WriteLine("Массив цифр числа:");
+        for (int i = 0; i < digitsCount; i++)
+        {
+            Console.WriteLine(digits[i]);
+        }
