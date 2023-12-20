1f570ab17b51c38e4397b33741a18ffdc91d87fa
Andrey Proskurin <>
2023-12-20 20:16:43 +0300

    Добавил решение

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задаем массив вещественных чисел
+        double[] numbers = { 2.5, 1.3, 4.7, 3.1, 2.9 };
+        double min = numbers[0];
+        double max = numbers[0];
+        for (int i = 1; i < numbers.Length; i++)
+        {
+            if (numbers[i] < min)
+                min = numbers[i];
+            
+            if (numbers[i] > max)
+                max = numbers[i];
+        }
+        double difference = max - min;
+        Console.WriteLine("Разница между максимальным и минимальным элементами массива: " + difference);
+    
