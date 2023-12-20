3de2491b588ea732422145eea994f3ef8b90c921
Andrey Proskurin <>
2023-12-20 20:44:52 +0300

    сделал третье решение

---
+++ Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿{
+        int[] array = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+        int count = 0;
+        
+        foreach (var element in array) {
+            if (element >= 20 && element <= 90) {
+                count++;
+            }
+        }
+        
+        Console.WriteLine("Количество элементов массива, значения которых лежат в отрезке от 20 до 90]. " + count);
+    }
